3740047cb50d2f8bd14a8629ea1b83aa64615c6b
Motti <=>
2016-11-14 12:50:30 -0500

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function addIngredient(recipe,ingredient,amount){
+
+		recipe[ingredient] = amount;
+		return recipe; 
+}
+
+function removeIngredient(recipe,ingredient){
+	delete recipe[ingredient];
+	return recipe;
+}
+
+function updateIngredient(recipe,ingredient,amount) {
+	recipe[ingredient] = amount;
+ 	return recipe;
+}
+			 
+
+
+function readRecipe(recipe){
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
+// Create a function readRecipe 
+// which accepts a recipe object 
+// as a parameter. The function 
+// should console.log a string of each
+// ingredient and the amount "this 
+// recipe calls for <amount> of <ingredient>"
